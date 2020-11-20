552dcac17d9babb15e1fc0a108993c4ec28809b8
Drenyash <kavlod12345@gmail.com>
2020-11-21 00:21:37 +0300

    Добавлен js фаил для реализации навигации

---
+++ source/js/script.js
@@ -0,0 +1,15 @@
+const burger = document.querySelector('.burger');
+const siteList = document.querySelector('.site-list');
+const mainNavHeader = document.querySelector('.main-nav__header');
+
+burger.addEventListener('click', function() {
+  if (this.classList.contains('burger--active')) {
+    this.classList.remove('burger--active');
+    siteList.classList.add('site-list--close');
+    mainNavHeader.classList.add('main-nav__header--close');
+  } else {
+    this.classList.add('burger--active');
+    siteList.classList.remove('site-list--close');
+    mainNavHeader.classList.remove('main-nav__header--close');
+  }
+})
