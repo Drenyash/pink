76144e7da04641ca821229d5720e51ef84d629f8
Drenyash <kavlod12345@gmail.com>
2020-11-20 20:01:53 +0300

    Настройка gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 const gulp = require("gulp");
 const plumber = require("gulp-plumber");
 const sourcemap = require("gulp-sourcemaps");
-const less = require("gulp-less");
+const sass = require("gulp-sass");
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
 const sync = require("browser-sync").create();
@@ -9,10 +9,10 @@ const sync = require("browser-sync").create();
 // Styles
 
 const styles = () => {
-  return gulp.src("source/less/style.less")
+  return gulp.src("source/sass/style.scss")
     .pipe(plumber())
     .pipe(sourcemap.init())
-    .pipe(less())
+    .pipe(sass())
     .pipe(postcss([
       autoprefixer()
     ]))
@@ -25,7 +25,7 @@ exports.styles = styles;
 
 // Server
 
-const server = (done) => {
+const server = () => {
   sync.init({
     server: {
       baseDir: 'source'
@@ -34,7 +34,6 @@ const server = (done) => {
     notify: false,
     ui: false,
   });
-  done();
 }
 
 exports.server = server;
@@ -42,7 +41,7 @@ exports.server = server;
 // Watcher
 
 const watcher = () => {
-  gulp.watch("source/less/**/*.less", gulp.series("styles"));
+  gulp.watch("source/sass/**/*.scss", gulp.series("styles"));
   gulp.watch("source/*.html").on("change", sync.reload);
 }
 
