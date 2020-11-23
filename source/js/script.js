128f82614dad569c847f7411263a7be52ae541fe
Drenyash <kavlod12345@gmail.com>
2020-11-23 22:49:22 +0300

    Стилизация и адаптив под планшеты и компы

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,20 @@
 const burger = document.querySelector('.burger');
 const siteList = document.querySelector('.site-list');
 const mainNavHeader = document.querySelector('.main-nav__header');
+const nav = document.querySelector('.main-nav');
+
+nav.style.background = 'none';
 
 burger.addEventListener('click', function() {
   if (this.classList.contains('burger--active')) {
     this.classList.remove('burger--active');
     siteList.classList.add('site-list--close');
     mainNavHeader.classList.add('main-nav__header--close');
+    nav.style.background = 'none';
   } else {
     this.classList.add('burger--active');
     siteList.classList.remove('site-list--close');
     mainNavHeader.classList.remove('main-nav__header--close');
+    nav.style.background = '#283645';
   }
 })
